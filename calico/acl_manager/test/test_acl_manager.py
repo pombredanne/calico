ec8bb334f968b8af17b39e7716a2919588ef5a83
Matt Dupre <matthew.dupre@metaswitch.com>
2015-01-02 17:00:54 +0000

    ACL Manager UT: Run ACL Manager in a separate process

--- calico/acl_manager/test/test_acl_manager.py
+++ calico/acl_manager/test/test_acl_manager.py
@@ -12,25 +12,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+from multiprocessing import Process
 import unittest
-import calico.acl_manager.acl_manager
 
-# Default config path.
-config_path = "calico/acl_manager/test/data/default.cfg"
+import calico.acl_manager.acl_manager as acl_manager
+
+# Default configuration file path.
+default_config = "calico/acl_manager/test/data/default.cfg"
 
 class TestMainline(unittest.TestCase):
+    """
+    Mainline tests for the whole of ACL Manager.
+
+    These tests run the whole of ACL Manager in a separate process, and verify
+    its mainline function.
+    """
+
+    def set_command_line_args(self, config_path):
+        """Set the command line arguments that ACL Manager will use"""
+        del sys.argv[1:]
+        sys.argv.extend(["-c", config_path])
+
+    def start_acl_manager(self):
+        self.acl_manager_process = Process(target = acl_manager.main)
+        self.acl_manager_process.start()
+
+    def stop_acl_manager(self):
+        self.acl_manager_process.terminate()
 
     def setUp(self):
-        pass
-        
+        self.acl_manager_process = None
+
     def tearDown(self):
-        pass
-        
+        if (self.acl_manager_process and self.acl_manager_process.is_alive()):
+            self.stop_acl_manager()
+
     def test_case1(self):
         self.assertEqual(1, 1)
-        
+
     def test_case2(self):
-        calico.acl_manager.acl_manager.main()
+        self.set_command_line_args(default_config)
+        self.start_acl_manager()
 
 if __name__ == '__main__':
     unittest.main()
